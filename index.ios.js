59a8ffeb4660fec28b80723f8ddc894ba9bdacb3
pchw <bump.luv@gmail.com>
2017-05-16 09:11:00 +0900

    :art:通信エラー時にエラーメッセージを出すようにした

--- index.ios.js
+++ index.ios.js
@@ -1,22 +1,23 @@
 import React, { Component } from 'react';
-import {
-  AppRegistry
-} from 'react-native';
+import { AppRegistry } from 'react-native';
 
 import { StackNavigator, TabNavigator } from 'react-navigation';
 
-const quickAnnict = TabNavigator({
-  Episode: {
-    screen: require('./js/TabHomeScreen').default,
-    path: /^callback/
+const quickAnnict = TabNavigator(
+  {
+    Episode: {
+      screen: require('./js/TabHomeScreen').default,
+      path: /^callback/
+    },
+    Program: {
+      screen: require('./js/TabProgramScreen').default
+    }
   },
-  Program: {
-    screen: require('./js/TabProgramScreen').default
+  {
+    containerConfig: {
+      URIPrefix: 'qani://'
+    }
   }
-}, {
-  containerConfig: {
-    URIPrefix: 'qani://'
-  }
-});
+);
 
 AppRegistry.registerComponent('quickAnnict', () => quickAnnict);
